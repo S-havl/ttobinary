79097deee9232ffb71fee3d2326ee8757730bde6
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-09-05 14:56:12 -0500

    feat(cypher): start state machine code for better software architecture

--- include/app/app.h
+++ include/app/app.h
@@ -1,3 +1,10 @@
 #pragma once
 
+typedef enum {
+    STATE_MENU,
+    STATE_ENCODE,
+    STATE_DECODE,
+    STATE_EXIT
+} appState_t;
+
 void app(void);
--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
+#include <stdio.h>
 #include <app/app.h>
 
 int main(void)
 {
-    app();
+    appState_t current_state = STATE_MENU;
+    int option = 0;
+
+    while (current_state != STATE_EXIT) {
+        app();
+    }
+
     return 0;
 }
